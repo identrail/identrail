1ea3848d8875ba21af4a7a988cae6f4afaabbfd0
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-04-28 19:41:24 +0100

    fix(web): retarget resources nav link to docs landing
    Signed-off-by: Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>

--- web/src/siteConfig.ts
+++ web/src/siteConfig.ts
@@ -3,7 +3,7 @@ export const siteLinks = {
   platform: '/platform',
   useCases: '/use-cases',
   solutions: '/solutions',
-  resources: '/resources',
+  resources: '/docs',
   company: '/company',
   pricing: '/pricing',
   docs: '/docs',
