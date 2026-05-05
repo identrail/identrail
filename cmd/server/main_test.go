24aa4026e4b9f8785f2b939cd714a6d0792e786d
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2026-05-05 14:46:28 +0100

    align go imports with module path
    Signed-off-by: Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Oluwatobi-Mustapha/identrail/internal/config"
+	"github.com/identrail/identrail/internal/config"
 )
 
 func TestRunCallsServerRuntime(t *testing.T) {
--- internal/providers/kubernetes/graph_test.go
+++ internal/providers/kubernetes/graph_test.go
@@ -5,8 +5,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/Oluwatobi-Mustapha/identrail/internal/domain"
-	"github.com/Oluwatobi-Mustapha/identrail/internal/providers"
+	"github.com/identrail/identrail/internal/domain"
+	"github.com/identrail/identrail/internal/providers"
 )
 
 func TestRelationshipResolverBuildsExpectedEdges(t *testing.T) {
